b4074b47205f1327297c1d575223059bf3067c0e
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-03-15 15:05:48 -0400

    Add search by name reducer

--- src/store/country/countrySlice.js
+++ src/store/country/countrySlice.js
@@ -1,14 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* eslint-disable max-len */
+/* eslint-disable no-param-reassign */
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const baseURL = 'https://restcountries.com/v3.1';
+
+const getCountry = createAsyncThunk('country/getCountry', async () => {
+  const endpoint = '/region/europe';
+  const url = baseURL + endpoint;
+  const { data } = await axios.get(url);
+  return data;
+});
 
 const initialState = {
-  country: [],
+  countries: [],
+  searchCountry: {},
 };
 
 const countrySlice = createSlice({
-  name: 'country',
+  name: 'countries',
   initialState,
-  reducers: {},
-  extraReducers: {},
+  reducers: {
+    searchByName: (state, { payload }) => {
+      const { countryName } = payload;
+      const country = state.countries.filter((country) => country.name.common === countryName);
+
+      const {
+        capital, flags, tld, startOfWeek, name, population,
+      } = country[0];
+
+      state.searchCountry = {
+        capital: capital[0], tld: tld[0], flags: flags.png, startOfWeek, oficialName: name.official, population,
+      };
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getCountry.fulfilled, (state, { payload }) => {
+      state.countries = payload;
+    });
+  },
 });
 
+export { getCountry };
+export const { searchByName } = countrySlice.actions;
 export default countrySlice.reducer;
