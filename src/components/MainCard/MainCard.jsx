d69ea548db853349b12000f5bead0744d8e5b93b
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-03-15 15:05:18 -0400

    Made main card dynamic

--- src/components/MainCard/MainCard.jsx
+++ src/components/MainCard/MainCard.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './MainCard.module.css';
 
-const MainCard = () => (
+const MainCard = ({ flag, name }) => (
   <div className={styles.container}>
     <div className={styles.map}>
-      <img src="/assets/img/map.png" alt="map" />
+      <img src={flag} alt="map" />
     </div>
     <div className={styles.continent}>
       <div>
-        <h3>Europe</h3>
-        <p>10 country</p>
+        <h3>{name}</h3>
       </div>
     </div>
   </div>
 );
+
+MainCard.propTypes = {
+  flag: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
 export default MainCard;
