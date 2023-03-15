44ee7f8647bfb30c9804afd20e0176048ccd43f8
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-03-14 21:44:10 -0400

    Add HeadCardList component

---
+++ src/components/HeadCardList/HeadCardList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './HeadCardList.module.css';
+
+const HeadCardList = () => (
+  <div className={styles.container}>
+    <p>Stat by country</p>
+  </div>
+);
+
+export default HeadCardList;
