74372fe615e33e49b1c2f483f9331b382f8519f1
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-03-15 19:44:16 -0400

    Made nvabar dynamic

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
 import { FiChevronLeft, FiSettings, FiMic } from 'react-icons/fi';
 
 import styles from './Navbar.module.css';
 
-const Navbar = () => (
-  <div className={styles.navbar}>
-    <div className={styles.return}>
-      <FiChevronLeft />
-      <p>2022</p>
+const Navbar = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.return}>
+        { pathname !== '/' && (
+        <Link style={{ color: '#fff', textDecoration: 'none' }} to="/">
+          <FiChevronLeft />
+        </Link>
+        )}
+        <p>2023</p>
+      </div>
+      <div className={styles.title}>
+        <p>
+          {pathname === '/' ? 'Continent' : 'Country' }
+          {' '}
+        </p>
+      </div>
+      <div className={styles.settings}>
+        <FiMic />
+        <FiSettings />
+      </div>
     </div>
-    <div className={styles.title}>
-      <p>most view</p>
-    </div>
-    <div className={styles.settings}>
-      <FiMic />
-      <FiSettings />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
